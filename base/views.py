4d63f7a65a0102af4069bc38d85ddd7542f3761e
Shambashib <shambashibmajumdar2000@gmail.com>
2022-07-13 17:25:59 +0530

    [ADD] backend building started, added models

--- base/views.py
+++ base/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
-
+from .models import Room
 
 # Create your views here.
-rooms = [
-    {'id':1, 'name':'Lets learn React!'},
-    {'id':2, 'name':'Design with me!'},
-    {'id':3, 'name':'Lets learn Java!'},
-    {'id':4, 'name':'Frontend Development!'},
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn React!'},
+#     {'id':2, 'name':'Design with me!'},
+#     {'id':3, 'name':'Lets learn Java!'},
+#     {'id':4, 'name':'Frontend Development!'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms} 
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+    # room = None
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
     context = {'room': room}
     return render(request, 'base/room.html', context)
