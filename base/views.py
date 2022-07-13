6ce02dda37cfc859b6c493d4ced8bf60cb70a15b
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-07-14 01:28:24 +0530

    [NEW] completed room creation form

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
 from .forms import RoomForm
 
@@ -26,7 +26,22 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm()
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
 
 
