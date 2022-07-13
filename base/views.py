b8e48f42cdd96b81e96f9d55d98c877011655136
Shambashib <shambashibmajumdar2000@gmail.com>
2022-07-13 16:42:42 +0530

    [ADD] added ginja templating

--- base/views.py
+++ base/views.py
@@ -3,11 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 rooms = [
-    
+    {'id':1, 'name':'Lets learn React!'},
+    {'id':2, 'name':'Design with me!'},
+    {'id':3, 'name':'Lets learn Java!'},
+    {'id':4, 'name':'Frontend Development!'},
 ]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms} 
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
