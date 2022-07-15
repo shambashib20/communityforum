310d83a74656c5dc0c210bc52ed4f19d2863390e
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-07-15 11:19:39 +0530

    feat: showing participants in a room

--- base/views.py
+++ base/views.py
@@ -85,6 +85,8 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -92,9 +94,10 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
